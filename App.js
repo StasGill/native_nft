8b8043a35211db0f7223fe88bbef2f1194970707
Stanislav Gil <stasgill@gmail.com>
2022-06-09 23:43:54 +0300

    work done

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./screens/Home";
+import { useFonts } from "expo-font";
+import Details from "./screens/Details";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  // const [loaded] = useFonts({
+  //   InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+  //   InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+  //   InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+  //   InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+  //   InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  //   BariolRegular: require("./assets/fonts/Bariol_Regular.otf"),
+  // });
+
+  // if (!loaded) return <Text>Loading</Text>;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/index.js
@@ -0,0 +1,22 @@
+import HomeHeader from "./HomeHeader";
+import NFTcard from "./NFTcard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+  HomeHeader,
+  NFTcard,
+  FocusedStatusBar,
+  RectButton,
+  CircleButton,
+  SubInfo,
+  People,
+  EndDate,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  DetailsDesc,
+};
